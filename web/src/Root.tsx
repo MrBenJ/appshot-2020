427fe27fb3d5a023385b5ece6ea8aa0f87be29c5
Ben Junya <bjunya@gmail.com>
2019-11-17 20:10:09 -0800

    Pretty index page

--- web/src/Root.tsx
+++ web/src/Root.tsx
@@ -5,8 +5,6 @@ import { Router } from '@reach/router';
 import FullscreenLayout from './components/layout/FullscreenLayout';
 import FlexContainer from './components/layout/FlexContainer';
 
-
-
 import IndexPage from './pages/Index';
 import Dashboard from './pages/Dashboard';
 
--- web/src/pages/Index/IndexPage.tsx
+++ web/src/pages/Index/IndexPage.tsx
@@ -4,7 +4,5 @@ import { RouteComponentProps } from '@reach/router';
 interface IndexPageProps extends RouteComponentProps {}
 
 export default function IndexPage(props: IndexPageProps) {
-  return (
-    <div className="flex-center flex1 flex-column">Index Page</div>
-  );
+  return <div className="flex-center flex1 flex-column">Index Page</div>;
 }
